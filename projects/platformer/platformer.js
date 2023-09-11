706535ced369ac1b64d5118d0ef8dab49edd8df4
Connor  Hill <Hill505@stcharles.k12.la.us>
2023-09-11 18:58:45 +0000

    fixing index

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,17 +43,18 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(200,500, 200, 20 );
+    createPlatform(400,700, 200, 20);
+    createPlatform(600, 600, 200, 20);
+    createPlatform(800 ,500, 200, 20);
+    createPlatform(1000, 600, 200, 20);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("steve",250,460,20,0.5);
+    createCollectable("grace",450,660,20,0.5);
 
 
     // TODO 3
