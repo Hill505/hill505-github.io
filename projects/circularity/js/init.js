4ce5b6505c40c60a676ed008381c4150272721f8
Connor  Hill <Hill505@stcharles.k12.la.us>
2023-11-09 20:04:42 +0000

    push

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -22,14 +22,24 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
         var circle;
-        var circles = [0];
+        var circles = [];
        
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle(){
+
         
-
+        // Code to draw a circle
+         circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+         physikz.addRandomVelocity(circle, canvas, 10, 10);
+         view.addChild(circle);
+         circles.push(circle);
+        }
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+    
+ 
+        for(var i = 0; i < 100; i++){
+            drawCircle();
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -41,11 +51,12 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+           for(var i = 0; i < circles.length; i++){
+            physikz.updatePosition(circles[i])
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+           game.checkCirclePosition(circles[i])
+           }
             // TODO 9 : Iterate over the array
            
             
@@ -64,9 +75,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+            if(circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if(circle.y > canvas.height){
+                circle.y = 0;
+            }
+            if(circle.y < 0){
+                circle.y = canvas.height;
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
@@ -92,3 +109,4 @@ if((typeof process !== 'undefined') &&
     // here, export any references you need for tests //
     module.exports = init;
 }
+globalThis.push
